b6b6ac06737c42bbe9d8c1acadf579fd95cbe12b
Renan "Firehawk" Lazarotto <renanlazarotto@gmail.com>
2025-01-28 08:08:45 -0300

    feat: Mostra o tempo máximo permitido por lei e aceita valores zerados como parâmetro

--- main.go
+++ main.go
@@ -13,9 +13,10 @@ import (
 )
 
 const (
-	FULL_TIME  = 9*time.Hour + 48*time.Minute
-	HALF_TIME  = 9*time.Hour + 30*time.Minute
-	SHORT_TIME = 9 * time.Hour
+	FULL_TIME    = 9*time.Hour + 48*time.Minute
+	HALF_TIME    = 9*time.Hour + 30*time.Minute
+	SHORT_TIME   = 9 * time.Hour
+	MAX_OVERTIME = 11*time.Hour + 48*time.Minute
 )
 
 type ClockIn struct {
@@ -39,8 +40,8 @@ func (c ClockIn) GetTime() time.Time {
 
 func (c ClockIn) Validate() error {
 	return validation.ValidateStruct(&c,
-		validation.Field(&c.StartHour, validation.Required.Error("não pode ser vazio")),
-		validation.Field(&c.StartMinute, validation.Required.Error("não pode ser vazio")),
+		validation.Field(&c.StartHour, validation.NotNil.Error("não pode ser vazio")),
+		validation.Field(&c.StartMinute, validation.NotNil.Error("não pode ser vazio")),
 	)
 }
 
@@ -70,22 +71,24 @@ func main() {
 	fullTime := clockIn.GetTime().Add(FULL_TIME)
 	halfTime := clockIn.GetTime().Add(HALF_TIME)
 	shortTime := clockIn.GetTime().Add(SHORT_TIME)
+	maxOvertime := clockIn.GetTime().Add(MAX_OVERTIME)
 
 	bold := lipgloss.NewStyle().Bold(true).Render
 	center := lipgloss.NewStyle().Align(lipgloss.Center)
 
 	t := table.New()
 	t.StyleFunc(func(row, col int) lipgloss.Style {
-		if col == 0 {
+		if col == 1 {
 			return center
 		}
 
 		return table.DefaultStyles(row, col)
 	})
 	t.Row(bold("Jornada"), bold("Saída"))
-	t.Row(bold("8:48"), fullTime.Format("15:04"))
-	t.Row("8:30", halfTime.Format("15:04"))
-	t.Row("8:00", shortTime.Format("15:04"))
+	t.Row(bold("10:48 (+2h)"), maxOvertime.Format("15:04"))
+	t.Row(bold("08:48"), fullTime.Format("15:04"))
+	t.Row("08:30", halfTime.Format("15:04"))
+	t.Row("08:00", shortTime.Format("15:04"))
 
 	t.Border(lipgloss.HiddenBorder())
 
